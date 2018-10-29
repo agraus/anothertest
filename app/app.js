b0f39847122a2de0c545ff3a42b83e8e6eac2f13
Agraus <agraus56@gmail.com>
2018-10-29 13:08:52 +0300

    added all draw functions

--- app/app.js
+++ app/app.js
@@ -1,4 +1,17 @@
 let canvas = document.querySelector("canvas");
 let context = canvas.getContext("2d");
-context.fillStyle = "red";
-context.fillRect(10, 10, 100, 50);
+context.strokeStyle = "maroon";
+context.lineWidth = "4";
+context.strokeRect(canvas.width/2 -60,200,80,200);
+
+
+function init(size) {
+    context.font = "20px SansSerif";
+    context.fillText(size+'%',canvas.width/2+30,canvas.height/2);
+    context.translate(0, canvas.height);
+    context.scale(1, -1);
+    context.fillStyle = "red";
+    context.fillRect(canvas.width/2 -58,202,76,size*2);
+
+}
+init(80);
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 
-echo "<body><canvas width='400' height='600'></canvas></body>";
+echo "<body><canvas width='600' height='600' style='border: 5px solid aqua'></canvas></body>";
 echo "<script type='text/javascript' src='/app/app.js'></script>";
 
 ?>
